b848331a072b0c005debf97483aa70f9cdf81884
USBST <134568903+USBST@users.noreply.github.com>
2023-05-25 08:24:42 +0530

    Newcls

---
+++ src/test/java/mavenpgms/Mavengitpgm.java
@@ -0,0 +1,19 @@
+package mavenpgms;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+
+public class Mavengitpgm {
+  @Test
+  public void f() {
+  }
+  @BeforeTest
+  public void beforeTest() {
+  }
+
+  @AfterTest
+  public void afterTest() {
+  }
+
+}

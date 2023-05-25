da64d137f11311712898979f865a9fa91077ec0b
USBST <134568903+USBST@users.noreply.github.com>
2023-05-25 08:04:35 +0530

    First code push

---
+++ src/test/java/mavenpgms/SearchPgm.java
@@ -0,0 +1,28 @@
+package mavenpgms;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+
+public class SearchPgm {
+	WebDriver driver;
+  @Test
+  public void Search() {
+	  driver.get("https://www.google.com");
+	  driver.findElement(By.name("q")).sendKeys("maven");
+  }
+  @BeforeTest
+  public void beforeTest() {
+	  driver= new ChromeDriver();
+	  driver.manage().window().maximize();
+  }
+
+  @AfterTest
+  public void afterTest() {
+	  driver.quit();
+  }
+
+}
